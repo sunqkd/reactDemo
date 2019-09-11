91aa9083f55aff47b643318bd45a1806ad4ed7ef
sunzhaoqkd <sunzhaoqkd@163.com>
2019-09-11 11:28:08 +0800

    update

--- src/components/Projectcontent.js
+++ src/components/Projectcontent.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
+import url from 'url';
+
 class Projectcontent extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { 
+            aid:''
+        };
     }
     render() {
         return (
             <div>
-                666
+                {this.state.aid}
             </div>
         );
+    }
+
+    componentDidMount(){
+        console.log(url.parse(this.props.location.search,true))
+
+        var aid = url.parse(this.props.location.search,true).query.aid;
+
+        this.setState({
+            aid:aid
+        })
     }
 }
 
